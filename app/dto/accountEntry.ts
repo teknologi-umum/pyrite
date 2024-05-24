85064441930a17557615a0e16ec06623aeeb4508
Reinaldy Rafli <aldy505@proton.me>
2024-05-24 11:11:51 +0700

    ci: setup nocodb

--- app/dto/accountEntry.ts
+++ app/dto/accountEntry.ts
@@ -7,7 +7,7 @@ export const accountEntrySchema = z.object({
   type: z.enum(AccountEntryType),
   idr_amount: z.string().transform((val) => new Decimal(val)),
   original_currency: z.enum(AccountEntryCurrency),
-  original_ammount: z.string().transform((val) => new Decimal(val)),
+  original_amount: z.string().transform((val) => new Decimal(val)),
   category: z.enum(AccountEntryCategory),
   description: z.string().max(500),
   payout: z.object({
