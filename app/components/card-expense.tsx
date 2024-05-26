f2b65f0782c3bddafe813d26db95d21ecd500e0d
Marvel Alexius <hades.marvel@gmail.com>
2024-05-26 18:08:08 +0800

    added expense detail page
    - added a href wrapper in expense card
    - installed new badge component from shadcn

--- app/components/card-expense.tsx
+++ app/components/card-expense.tsx
@@ -34,21 +34,23 @@ export function CardExpense({
         <ul>
           {expenses.map((expense) => (
             <li className="my-2" key={expense.transaction_code}>
-              <div className="flex justify-between">
-                <h3 className="text-lg font-medium">{expense.description}</h3>
-                <h3 className="text-lg font-medium">
-                  {new Intl.NumberFormat("id-ID", {
-                    style: "currency",
-                    currency: "IDR",
-                  }).format(expense.idr_amount.toNumber())}
-                </h3>
-              </div>
+              <a href={`/expenses/123`}>
+                <div className="flex justify-between">
+                  <h3 className="text-lg font-medium">{expense.description}</h3>
+                  <h3 className="text-lg font-medium">
+                    {new Intl.NumberFormat("id-ID", {
+                      style: "currency",
+                      currency: "IDR",
+                    }).format(expense.idr_amount.toNumber())}
+                  </h3>
+                </div>
 
-              <p className="text-sm font-light">Category: {expense.category}</p>
-              <p className="text-sm font-light">
-                To: {expense.payout.recipient_name} -{" "}
-                {expense.timestamp.toDateString()}
-              </p>
+                <p className="text-sm font-light">Category: {expense.category}</p>
+                <p className="text-sm font-light">
+                  To: {expense.payout.recipient_name} -{" "}
+                  {expense.timestamp.toDateString()}
+                </p>
+              </a>
               <Separator className="my-2" />
             </li>
           ))}
